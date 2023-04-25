76dc5920f9471d7da7d2cba23b3eb0c8e92d8fdc
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2023-04-25 01:10:56 -0300

    Relógio feito

---
+++ JS/relogio.js
@@ -0,0 +1,23 @@
+const hourHand = document.getElementById("hour-hand");
+const minuteHand = document.getElementById("minute-hand");
+const secondHand = document.getElementById("second-hand");
+
+function getTime(){
+  const date = new Date();
+
+  return{
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+  };
+}
+
+
+setInterval(()=> {
+  const time = getTime();
+
+  secondHand.style.transform = `translate(0, -50%) rotate(${time.seconds*6}deg)`;
+  minuteHand.style.transform = `translate(0, -50%) rotate(${time.minutes*6}deg)`;
+  hourHand.style.transform = `translate(0, -50%) rotate(${time.hours*30}deg)`;
+  
+}, 1000);
