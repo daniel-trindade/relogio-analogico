330a501e577477cc3cf1e31c9b4b49e1e78a8500
Daniel Trindade <85046298+daniel-trindade@users.noreply.github.com>
2024-01-19 00:21:28 -0300

    Complemento

--- JS/relogio.js
+++ JS/relogio.js
@@ -1,6 +1,10 @@
 const hourHand = document.getElementById("hour-hand");
 const minuteHand = document.getElementById("minute-hand");
 const secondHand = document.getElementById("second-hand");
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
 
 function getTime(){
   const date = new Date();
@@ -12,12 +16,20 @@ function getTime(){
   };
 }
 
-
 setInterval(()=> {
   const time = getTime();
 
   secondHand.style.transform = `translate(0, -50%) rotate(${time.seconds*6}deg)`;
   minuteHand.style.transform = `translate(0, -50%) rotate(${time.minutes*6}deg)`;
   hourHand.style.transform = `translate(0, -50%) rotate(${time.hours*30}deg)`;
+  
+}, 1000);
+
+setInterval(()=> {
+  const time = getTime();
+
+  sec.innerHTML = time.seconds;
+  min.innerHTML = time.minutes;
+  hour.innerHTML = time.hours;
   
 }, 1000);
